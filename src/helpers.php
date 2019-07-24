d92324fc2347f73fca6eb4ae25a688361a50c05c
Vladimir Zabara <vzabara@finbridgecorp.com>
2019-07-24 16:05:22 +0300

    Helper is added

--- src/helpers.php
+++ src/helpers.php
@@ -34,19 +34,3 @@ if (! function_exists('public_path')) {
         return app()->make('path.public') . ($path ? DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR) : $path);
     }
 }
-
-if ( ! function_exists( 'flog' ) ) {
-    function _var_dump( $var ) {
-        ob_start();
-        print_r( $var );
-        $v = ob_get_contents();
-        ob_end_clean();
-
-        return $v;
-    }
-
-    function flog( $var ) {
-        file_put_contents( storage_path('logs') . '/log-' . date( 'Y-m-d' ) . '.txt', '+---+ ' . date( 'H:i:s d-m-Y' ) . ' +-----+' . PHP_EOL . _var_dump( $var ) . PHP_EOL . PHP_EOL, FILE_APPEND );
-    }
-}
-
